621f1c74b0c542935b0af6fb1c53e1dcc4118963
Matt Corbin <mcorbin93@gmail.com>
2021-09-26 13:37:16 -0400

    day 4 solutions

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,15 @@
+extern crate md5;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_key = "yzbqklnj";
+    let mut test: u64 = 0;
+    loop {
+        let digest = md5::compute(format!("{0}{1}", secret_key, test));
+        let hex = format!("{:x}", digest);
+        if hex.starts_with("000000") {
+            break
+        }
+        test += 1;
+    }
+    println!("Answer: {0}", test);
 }
