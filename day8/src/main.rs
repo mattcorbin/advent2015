c413eadd7610b4a728cf4b47470fb5ad454981bd
Matt Corbin <mcorbin93@gmail.com>
2021-09-30 17:20:51 -0400

    day 8 solutions

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
+fn count_characters(line: &str) -> usize {
+    let mut count = 0;
+    let mut escape_next = false;
+    let mut skip = 0;
+    for c in line.chars() {
+        if skip > 0 {
+            skip -= 1;
+        } else if escape_next {
+            escape_next = false;
+            if c == 'x' {
+                skip = 2
+            }
+        } else if c == '\\' {
+            escape_next = true;
+            count += 1;
+        } else {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn escape_again(line: &str) -> usize {
+    line.replace("\\", "\\\\").replace("\"","\\\"").len() + 2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("input.txt should exist");
+    let mut total_chars = 0;
+    let mut memory_values = 0;
+    let mut double_escaped = 0;
+    for line in input.lines() {
+        total_chars += line.len();
+        memory_values += count_characters(line.trim_matches('\"'));
+        double_escaped += escape_again(line);
+    }
+    println!("difference in characters p1: {0}", total_chars - memory_values);
+    println!("difference in characters p2: {0}", double_escaped - total_chars);
 }
